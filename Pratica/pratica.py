d4723cb5c1ac28eee718e02fcf4b6cf291d7fb9f
Deiner Rodrigues de Souza <deiner.souza@win.ufu.br>
2025-08-06 18:02:33 -0300

    bot de navegação criado

--- Pratica/pratica.py
+++ Pratica/pratica.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+
+
+
 driver = webdriver.Chrome()
 driver.get("https://fau.softcomshop.com.br/auth/login")
 
@@ -19,28 +22,34 @@ input_senha.send_keys(senha)
 button_login.click()
 
 driver.implicitly_wait(5)
-indice = 423
+
 lista = []
-for i in range(39):
+
+for indice in range(424,463):
     try:
         driver.get(f"https://fau.softcomshop.com.br/nfe2/{indice}/editar")
-        driver.implicitly_wait(5)
+        
         try:
             button_not_show = driver.find_element(By.CSS_SELECTOR, ".content-do-not-show-button")
+            button_not_show.click()
         except Exception as e:
-            print ("")
-        button_not_show.click()
+            with open("log.txt", "a", encoding="utf-8") as log:
+                log.writelines(f"Erro: {str(e)}")
+            print ("Botão não encontrado")
+        
         html = driver.page_source
-        soup = BeautifulSoup(html, "html.parser")
+        soup = BeautifulSoup(html,"html.parser")
         dados = {
         "chave_nota" : soup.find("span", class_="access_key_nfe").text,
         "numero da nota" : soup.find("span", class_="number_nfe").text,
         #"cpf" : soup.find("input", class_="form-control mask_cpf_cnpj")["value"],
         "valor da nota": soup.find("span", id="total_produto_valor").text,
         "CFOP":soup.find("input", id="auto_natureza")["value"]}
-        indice +=1
-        lista.extend(dados)
-    except:
+
+        lista.append(dados)
+    except Exception as e:
+        with open("log.txt", "a", encoding="utf-8") as log:
+            log.writelines(f"Erro: {str(e)}")
         print("não foi possível")
 
 df = pd.DataFrame(lista)
