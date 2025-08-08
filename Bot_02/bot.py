02dcff2547e0165c317f0004955da738054a0a32
Deiner Rodrigues de Souza <deiner.souza@win.ufu.br>
2025-08-07 21:27:05 -0300

    bot extração de metadados de livro por ISBN

---
+++ Bot_02/bot.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome()
+driver.get("https://www.buscaisbn.com.br")
+
+isbn_list = [9788570782533]  # Ou use: df = pd.read_csv("ISBN_list")
+
+for isbn in isbn_list:
+    input_text_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "search"))
+    )
+    input_text_box.clear()  # Limpa o campo antes de digitar
+    input_text_box.send_keys(isbn)
+
+    submit_button = driver.find_element(By.XPATH, "/html/body/main/div[1]/div/div/div[2]/button")
+    submit_button.click()
+
+    # Espera até que o resultado do ISBN seja carregado (ex: uma tag <p> com texto)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, """//*[@id="result"]/div/div[2]"""))
+    )
+
+    data = {"TITLE":driver.find_element(By.XPATH, """//*[@id="result"]/div/div[2]/div/h4""").text,
+            "AUTHOR":driver.find_element(By.XPATH,"""//*[@id="result"]/div/div[2]/div/div/p""").text,
+            "ISBN":driver.find_element(By.XPATH, """//*[@id="result"]/div/div[2]/div/p""").text,
+            "THEME1":driver.find_element(By.XPATH, """//*[@id="result"]/div/div[3]/span[1]""").text,
+            "THEME2":driver.find_element(By.XPATH, """//*[@id="result"]/div/div[3]/span[2]""").text,
+            "THEME3":driver.find_element(By.XPATH, """//*[@id="result"]/div/div[3]/span[3]""").text,
+            "PUBLISHER":driver.find_element(By.XPATH, """//*[@id="book_publisher"]""").text,
+            "BOOK_YEAR":driver.find_element(By.XPATH, """//*[@id="book_year"]""").text,
+            "BOOK_PAGE_COUT":driver.find_element(By.XPATH, """//*[@id="book_page_count"]""").text
+    } 
+    
+    
+
+    
+
+    print(data)
+
+
+driver.quit()
